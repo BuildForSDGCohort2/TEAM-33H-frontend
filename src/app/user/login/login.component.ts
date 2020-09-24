58bebe304d12570eb9d00111e2830d185edd378d
L. Theodore Obonye <ltobonye@gmail.com>
2020-09-24 08:24:24 +0200

    something

--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -66,7 +66,8 @@ export class LoginComponent implements OnInit {
                     this.router.navigate([this.returnUrl]);
                 },
                 error => {
-                    this.alertService.error(error.error.error);
+                    this.alertService.error(error);
+                    console.log(error);
                     this.loading = false;
                 });
     }
