947afbfdebcec5e329ee4fa788bb5a07c4058bae
L. Theodore Obonye <ltobonye@gmail.com>
2020-09-25 14:12:40 +0200

    add drop down for selecting wallets

--- src/app/account/account.module.ts
+++ src/app/account/account.module.ts
@@ -14,6 +14,7 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatCardModule } from '@angular/material/card';
+import { MatSelectModule } from '@angular/material/select';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatInputModule } from '@angular/material/input';
 import { WalletComponent } from './wallet/wallet.component';
@@ -46,7 +47,8 @@ import { LogoutComponent } from './logout/logout.component';
     MatInputModule,
     MatFormFieldModule,
     MatTableModule,
-    MatPaginatorModule
+    MatPaginatorModule,
+    MatSelectModule
   ]
 })
 export class AccountModule { }
